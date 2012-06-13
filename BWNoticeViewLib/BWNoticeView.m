2bd9282ecde36558b96d72713d330f10d938db2a
Garrett Richards <grichards@seamlessweb.com>
2012-06-13 13:39:50 -0600

    removed UILabel extension, was not needed

--- BWNoticeViewLib/BWNoticeView.m
+++ BWNoticeViewLib/BWNoticeView.m
@@ -32,61 +32,6 @@
 
 @end
 
-@implementation UILabel (Extensions)
-
-- (NSArray *)lines
-{
-    if([self.text length] > 0)
-    {
-        NSMutableArray *lines = [NSMutableArray arrayWithCapacity:10];
-        NSCharacterSet *wordSeparators = [NSCharacterSet whitespaceAndNewlineCharacterSet];
-        NSString *currentLine = self.text;
-        int textLength = [self.text length];
-
-        NSRange rCurrentLine = NSMakeRange(0, textLength);
-        NSRange rWhitespace = NSMakeRange(0, 0);
-        NSRange rRemainingText = NSMakeRange(0, textLength);
-        BOOL done = NO;
-
-        while (NO == done)
-        {
-            // determine the next whitespace word separator position
-            rWhitespace.location = rWhitespace.location + rWhitespace.length;
-            rWhitespace.length = textLength - rWhitespace.location;
-            rWhitespace = [self.text rangeOfCharacterFromSet:wordSeparators options:NSCaseInsensitiveSearch range:rWhitespace];
-
-            if (NSNotFound == rWhitespace.location)
-            {
-                rWhitespace.location = textLength;
-                done = YES;
-            }
-
-            NSRange rTest = NSMakeRange(rRemainingText.location, rWhitespace.location - rRemainingText.location);
-            NSString *textTest = [self.text substringWithRange:rTest];
-            CGSize sizeTest = [textTest sizeWithFont:self.font forWidth:1024.0 lineBreakMode:UILineBreakModeWordWrap];
-
-            if (sizeTest.width > self.bounds.size.width)
-            {
-                [lines addObject:[currentLine stringByTrimmingCharactersInSet:wordSeparators]];
-                rRemainingText.location = rCurrentLine.location + rCurrentLine.length;
-                rRemainingText.length = textLength - rRemainingText.location;
-                continue;
-            }
-
-            rCurrentLine = rTest;
-            currentLine = textTest;
-        }
-
-        [lines addObject:[currentLine stringByTrimmingCharactersInSet:wordSeparators]];
-
-        return lines;
-    }
-    else
-        return nil;
-}
-@end
-
-
 @interface UIColor (BWNoticeViewColors)
 @end
 
